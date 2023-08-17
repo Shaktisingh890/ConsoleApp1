dbee2de57c571d209132bb1719f80d5f0f5f90a0
Shakti <shaktipp890@gmail.com>
2023-08-17 15:17:35 +0530

    1st commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+     public partial class Sum
+    {
+        static void Main(string[] args)
+        {
+            Sum calc=new Sum();
+            calc.Multi(12,23);
+            calc.Sum1(70,80);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp1/Sum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+     public partial class Sum
+    {
+        public void Sum1(int a, int b)
+        {
+            Console.WriteLine($"Sum method file Sum {a+b}" );
+        }
+    }
+}
